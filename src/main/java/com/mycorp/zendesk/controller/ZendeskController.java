46bc7f0d9428a6a9b32bae8ef5381732004ab397
jonycayon <jonycayon@gmail.com>
2018-03-25 19:48:37 +0200

    Controller + Swagger

--- src/main/java/com/mycorp/zendesk/controller/ZendeskController.java
+++ src/main/java/com/mycorp/zendesk/controller/ZendeskController.java
@@ -1,14 +1,27 @@
 package com.mycorp.zendesk.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mycorp.zendesk.service.ZendeskService;
+
+import util.datos.UsuarioAlta;
 
 @RestController
 public class ZendeskController {
 
-	@RequestMapping("/")
-    String getGreeting() {
-        return "Hello World!";
+	@Autowired
+	private ZendeskService zendeskService;
+	
+	@RequestMapping(value="/ticket", method=RequestMethod.POST)
+	public String altaTicket(
+			@RequestBody UsuarioAlta usuarioAlta, 
+			@RequestHeader(value="User-Agent") String userAgent) {
+        return zendeskService.altaTicketZendesk(usuarioAlta, userAgent);
     }
 	
 }
